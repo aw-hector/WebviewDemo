8fe1a1e0fa6e9c2cd0804bfbb3d18e9bc0599c12
Hector Huang <hector.huang@airwallex.com>
2024-01-25 09:20:57 +0800

    chore: first commit

---
+++ app/src/main/java/com/example/webviewtest/SecondFragment.kt
@@ -0,0 +1,47 @@
+package com.example.webviewtest
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import androidx.navigation.fragment.findNavController
+import com.example.webviewtest.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        WebView.setWebContentsDebuggingEnabled(true)
+        arguments?.getString("webviewUrl")?.let {
+            val webView = binding.webview
+            webView.loadUrl(it)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
